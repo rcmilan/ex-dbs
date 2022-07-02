51b3459da30b9dffdcd1a850eb5d4abc9b7afde0
Ricardo Milan <ricardo@fluencypass.com>
2022-07-02 18:56:15 -0300

    add person

--- app/Controllers/GraphController.cs
+++ app/Controllers/GraphController.cs
@@ -1,5 +1,6 @@
 ﻿using infra.graph;
 using Microsoft.AspNetCore.Mvc;
+using Neo4jClient;
 
 namespace app.Controllers
 {
@@ -7,19 +8,59 @@ namespace app.Controllers
     [ApiController]
     public class GraphController : ControllerBase
     {
-        public GraphController()
-        {
+        private readonly GraphClient _graphClient;
 
+        public GraphController(GraphDbClient graphDbClient)
+        {
+            this._graphClient = graphDbClient.Client;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        [HttpPost("follow")]
+        public async Task<IActionResult> Follow(Guid targetId, Guid followerId)
         {
-            Person x = new("name", "account");
+            var target = await Get(targetId);
+            var follower = await Get(followerId);
 
-            await Graph.addPerson(x);
+            await _graphClient
+                .Cypher
+                .Merge("(target:Person $target)").WithParam("target", target)
+                .Merge("(follower:Person $follower)").WithParam("follower", follower)
+                .Create("(follower)-[r: Follows]->(target)")
+                .ExecuteWithoutResultsAsync();
 
             return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(string name, string account)
+        {
+            Models.Person person = new(Guid.NewGuid(), name, account);
+
+            await _graphClient
+                .Cypher
+                .Create("(p:Person $person)")
+                .WithParam("person", person)
+                .ExecuteWithoutResultsAsync();
+
+            return Ok(person);
+        }
+
+        [HttpGet("{personId}")]
+        private async Task<IActionResult> Get(Guid personId)
+        {
+            var result = await GetById(personId);
+
+            return Ok(result);
+        }
+
+        private async Task<Models.Person?> GetById(Guid personId)
+        {
+            var targetAccount = await _graphClient.Cypher.Match("(p:Person)")
+                            .Where((Models.Person p) => p.Id.Equals(personId))
+                            .Return(p => p.As<Models.Person>())
+                            .ResultsAsync;
+
+            return targetAccount.ToList().FirstOrDefault();
         }
     }
 }
---
+++ infra.graph/GraphDbClient.fs
@@ -0,0 +1,9 @@
+﻿namespace infra.graph
+
+open System
+open Neo4jClient
+
+type GraphDbClient(connString : Uri, user : string, password : string) = 
+    let client = new GraphClient(connString, user, password)
+
+    member this.Client = client;
