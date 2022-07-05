8f041dd936e139e658ca7c0b7faa2baf347ba43f
Ricardo Silva Milan <13418519+rcmilan@users.noreply.github.com>
2022-07-05 10:18:00 -0300

    Fsharp/graph (#6)
    * adicionando infra graph
    
    * implementando client
    
    * docker compose
    
    * conn string
    
    * implementando métodos de nodes
    
    * compilando
    
    * cleanup
    
    * add person
    
    * add person
    
    * GetById
    
    * follower follows target
    
    * create unique
    
    * Module Dependency
    
    Co-authored-by: Ricardo Milan <ricardo@fluencypass.com>

--- infra.graph/GraphDbClient.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace infra.graph
-
-open System
-open Neo4jClient
-
-type GraphDbClient(connString : Uri, user : string, password : string) = 
-    let client = new GraphClient(connString, user, password)
-
-    member this.Client = client;
